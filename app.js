3a0b77b18b32aa15ac617677291b2a95de3e2218
Mariano <petotronco@gmail.com>
2021-05-18 18:33:30 -0300

    not found handler

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
-const { logger } = require('./configuration') 
+const { logger } = require('./configuration');
+const createError = require('http-errors');
 
 const middlewares = require('./middlewares');
 const routes = require('./routes');
 
 const app = express();
 
+// en error graves, el servidor se detiene, "no database, no app"
 process.on('unhandledRejection', (reason) => {
   logger.error(reason);
   process.exit(1);
-})
+});
 
 // middlewares
 middlewares(app);
 
 // routes 
 routes(app);
+
+// ultima opcion:
+app.use((req, res, next) => {
+  const error = createError(404);
+  res.status(error.statusCode).send(error.message);
+});
 
 module.exports = app;
