cc013da8f93b65407ad6466a96a74fbd9cda05c0
Mariano <petotronco@gmail.com>
2021-05-18 18:01:30 -0300

    get all movies

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,5 +1,21 @@
+const { dbCon } = require('../configuration')
+
 const getMovies = (req, res, next) => {
-  res.send('wait for movies');
+
+  const pageNum = parseInt(req.params.page);
+
+  if(isNaN(pageNum)){
+    return res.status(400).send('Bad request');
+  }
+
+  const moviesToSkip = (pageNum - 1) * 10;
+
+  dbCon('movies', async(db) => {
+    // queremos mostrar 10 movies por pagina
+    const movies = await db.find({}).skip(moviesToSkip).limit(10).toArray();
+    res.json(movies)
+  })
+
 };
 
 module.exports = {
