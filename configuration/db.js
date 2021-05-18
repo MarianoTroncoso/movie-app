bb3dbda1d5c5f0c43e93376fe547b9b26a2f02fa
Mariano <petotronco@gmail.com>
2021-05-18 17:32:20 -0300

    link morgan with winston; mongodb conection

---
+++ configuration/db.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+const _uri = 'mongodb+srv://admin:benji123@movieapp.shikt.mongodb.net/sample_mflix?retryWrites=true&w=majority'
+
+const dbCon = (coll, cb) => {
+  MongoClient.connect(_uri, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  })
+  .then( async client => { 
+    const db = client.db('sample_mflix').collection(coll);
+    await cb(db);
+    client.close();
+  })
+  .catch()
+}
+
+dbCon('movies', async (db) => {
+  const movie = await db.findOne();
+  console.log(movie)
+})
+
+module.exports = dbCon
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,8 @@
-// parametro: express app
 const morgan = require('morgan');
+const { logger } = require('../configuration');
 
 module.exports = ( app ) => {
-  app.use(morgan('dev'));
+  app.use(morgan('combined', {
+    stream: logger.stream
+  }));
 };
