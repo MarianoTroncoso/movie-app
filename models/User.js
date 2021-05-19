21c619a5474aa54331af7f537d6a8928a3ce3e71
Mariano <petotronco@gmail.com>
2021-05-19 08:55:50 -0300

    check user existence

--- models/User.js
+++ models/User.js
@@ -12,22 +12,58 @@ class User {
     });
   };
 
+  // el usuario que se quiere guardar ya existe?
+  checkExistence(){
+    return new Promise( (resolve, reject) => {
+      dbCon('users', async (db) => {
+
+        try {
+          
+          // por username o email
+          const user = await db.findOne({'$or': [{username: this.userData['username']}, {email: this.userData['email']}]});
+
+          if(!user){
+            // no existe el usuario, guardo
+            resolve({
+              check: false
+            });
+          } else if(this.userData['username'] === user.username){
+            // existe el username
+            resolve({
+              check: true,
+              message: 'this username is already in use'
+            })
+          } else if(this.userData['email'] === user.email){
+            // existe el email
+            resolve({
+              check: true,
+              message: 'this email es already in use'
+            });
+          };
+
+        } catch (error) {
+            reject(error);
+        }
+      });
+    });
+  };
+
   // validar la informacion antes de desperdiciar tiempo creando una nueva instancia
   static validate(userData){
-    // de la validación
     return userValidator.validate(userData);
-    // console.log(result.error.message);
   }
 };
 
-const userData = {
+const user = new User({
   username: 'marianot',
-  email: 'petotronco@gmail.com',
+  email: 'messi@gmail.com',
   password: 'Mariano-1234',
   first_name: 'Mariano',
   last_name: 'Troncoso'
-};
+});
 
-// si solo devuelve value, todo bien
-const validation = User.validate(userData);
-console.log(validation)
+user.checkExistence()
+.then( check => {
+  console.log(check)
+})
+.catch( err => console.log(err))
